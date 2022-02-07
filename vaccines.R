d3f9a93088391d290d46cc47ac61ae948c4c07d9
rg <rishigoutam@users.noreply.github.com>
2022-02-06 19:33:45 -0800

    Update vaccines.R

--- vaccines.R
+++ vaccines.R
@@ -1,7 +1,8 @@
-setwd("/Users/rishi/Documents/NYCDSA/Modules/vaccines")
+setwd("~/vaccines")
 library(tidyverse)
-covid <- read.csv("Vaccines.gov__COVID-19_vaccinating_provider_locations.csv")
-flu <- read.csv("Vaccines.gov__Flu_vaccinating_provider_locations.csv")
 
+
+covid <- tbl_df(read.csv("./data/Vaccines.gov__COVID-19_vaccinating_provider_locations.csv"))
+flu <- tbl_df(read.csv("./data/Vaccines.gov__Flu_vaccinating_provider_locations.csv"))
 both <- inner_join(flu, covid, by = "provider_location_guid") # TODO fix cols
 View(both)
