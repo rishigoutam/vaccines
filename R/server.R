e39e7bca94ef567a22f4540c317cca0867375185
rg <rishigoutam@users.noreply.github.com>
2022-02-11 00:07:18 -0800

    Format DT for display purposes

--- R/server.R
+++ R/server.R
@@ -8,6 +8,17 @@ shinyServer(function(input, output) {
                        radius = 3, stroke = FALSE, fillOpacity = 0.75)
   })
 
-  # Display data (covid)
-  output$covid <- DT::renderDataTable(covid) # TODO select cols
+  # Display table using DT
+  # see: https://rstudio.github.io/DT/
+  output$covid <- DT::renderDataTable({
+    covid_display <- covid %>%
+      relocate(name, city, state, zip, phone, insurance_accepted, walkins_accepted, Moderna, Pfizer, Pfizer_child, Janssen) %>%
+      ungroup() %>%
+      select(name, city, state, zip, phone, insurance_accepted, walkins_accepted, Moderna, Pfizer, Pfizer_child, Janssen)
+
+    covid_display #return
+  },
+  filter = 'top',
+  rownames = FALSE,
+  colnames = c("Vaccine Provider", "City", "State", "ZIP", "Phone No.", "Accepts Insurance", "Walk-Ins Allowed", "Moderna (18+) In Stock", "Pfizer (12+) In Stock", "Pfizer (5-11) In Stock", "J&J Janssen (18+) In Stock"))
 })
