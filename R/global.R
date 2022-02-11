cc698e321154aa34b358c918cd6288117c40e4d8
rg <rishigoutam@users.noreply.github.com>
2022-02-11 11:43:51 -0800

    Refactor provider popup info
    - Added data sources (urls)
    - Refactored popup into server.R
    - prettify popup
    - Added icon markers

--- R/global.R
+++ R/global.R
@@ -13,11 +13,14 @@ DEBUG <- TRUE
 # Load data ---------------------------------------------------------------
 
 # CDC covid vaccine provider locations
+# https://data.cdc.gov/Vaccinations/Vaccines-gov-COVID-19-vaccinating-provider-locatio/5jp2-pgaw
 covid <- read_csv("../data/Vaccines.gov__COVID-19_vaccinating_provider_locations.csv")
 
-# NYTimes Covid 19 data: https://github.com/nytimes/covid-19-data
+# NYTimes Covid 19 data
+# https://github.com/nytimes/covid-19-data
 masks <- read_csv("../data/mask-use-by-county.csv")
 
+# WA DOH Dashboard
 # https://www.doh.wa.gov/Emergencies/COVID19/DataDashboard#dashboard
 
 # Data Cleaning -----------------------------------------------------------
@@ -116,14 +119,4 @@ covid <- covid %>%
   filter(zip %in% c("98604", "98101", "11225", "19607"))
 
 
-# Create hover info for each provider -------------------------------------
 
-covid <- covid %>%
-  mutate(provider_popup = str_glue(.na = "",
-          '<strong>Name: </strong>{name}',
-          '<br><strong>Address: </strong>',
-          '<br>{street1} {street2}',
-          '<br>{city}, {state} {zip}',
-          '<br><strong>Phone: </strong>{phone}',
-          '<br><strong>In Stock: </strong>{ifelse (any(Moderna, Pfizer, Pfizer_child, Janssen), "Yes", "No")}',
-          '<br><strong>Walk-Ins Allowed: </strong>{ifelse (walkins_accepted, "Yes", "No")}'))
--- R/server.R
+++ R/server.R
@@ -1,11 +1,29 @@
 function(input, output) {
+  # Create information popup for each provider
+  covid <- covid %>%
+    mutate(provider_popup = str_glue(.na = "",
+          '<div class="popup">',
+           '<strong>Name: </strong>{name}',
+           '<div class="address"><br><strong>Address: </strong>',
+           '<br>{street1} {street2}',
+           '<br>{city}, {state} {zip}</div>',
+           '<br><strong>Phone: </strong>{phone}',
+           '<div class = "stock"><br><strong>In Stock: </strong>',
+           '<span><br>J&J Janssen (18+): </span><span>{ifelse(Janssen, "Yes", "No")}</span>',
+           '<br><span>Moderna (18+): <span>{ifelse (Moderna, "Yes", "No")}</span>',
+           '<br><span>Pfizer (12+): <span>{ifelse(Pfizer, "Yes", "No")}</span>',
+           '<br><span>Pfizer (5-11): <span>{ifelse (Pfizer_child, "Yes", "No")}</span></div>',
+           '<br><strong>Walk-Ins Allowed: </strong>{ifelse (walkins_accepted, "Yes", "No")}',
+          '</div>'))
 
   # Leaflet map
   output$cfmap <- renderLeaflet({
     leaflet(covid) %>%
       addTiles() %>%
-      addMarkers(data = covid, lat = ~latitude, lng = ~longitude,
-                 popup = ~provider_popup, clusterOptions = markerClusterOptions())
+      addAwesomeMarkers(data = covid, lat = ~latitude, lng = ~longitude,
+                        popup = ~provider_popup, clusterOptions = markerClusterOptions(),
+                        icon = awesomeIcons(icon = 'ion-medkit', iconColor = 'darkgreen',
+                                            library = 'ion', markerColor = 'green'))
   })
 
   # Display table using DT
@@ -20,5 +38,9 @@ function(input, output) {
   },
   filter = 'top',
   rownames = FALSE,
-  colnames = c("Vaccine Provider", "City", "State", "ZIP", "Phone No.", "Accepts Insurance", "Walk-Ins Allowed", "Moderna (18+) In Stock", "Pfizer (12+) In Stock", "Pfizer (5-11) In Stock", "J&J Janssen (18+) In Stock"))
+  colnames = c("Vaccine Provider", "City", "State", "ZIP", "Phone No.",
+               "Accepts Insurance", "Walk-Ins Allowed",
+               "Moderna (18+) In Stock", "Pfizer (12+) In Stock",
+               "Pfizer (5-11) In Stock", "J&J Janssen (18+) In Stock"))
 }
+
