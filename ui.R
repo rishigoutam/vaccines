be8432cd50a0702f9706c0d078c7557ace269f6b
rg <rishigoutam@users.noreply.github.com>
2022-02-11 23:29:12 -0800

    Add a fidget spinner
    -Add a spinner for loading the map data

--- ui.R
+++ ui.R
@@ -1,11 +1,12 @@
 library(shinydashboard)
+library(shinycssloaders)
 
 dashboardPage(
   dashboardHeader(title = "COVID-19 Dashboard"),
   dashboardSidebar(collapsed = TRUE),
   dashboardBody(
     tabsetPanel(
-      tabPanel("Map", leafletOutput("cfmap", height = 600)),
+      tabPanel("Map", withSpinner(leafletOutput("cfmap", height = "600px"), size = 3)),
       tabPanel("Data", DT::dataTableOutput("covid")),
       tabPanel("About", includeMarkdown("./README.md"))
       )
