f9f1355758294613dfb585a369bcc933d1ccf1d7
rg <rishigoutam@users.noreply.github.com>
2022-02-12 09:16:34 -0800

    Created report generator stub

--- server.R
+++ server.R
@@ -1,9 +1,11 @@
+# Vaccine Shiny app server
 function(input, output) {
   # Create map using Leaflet
   # see: https://rstudio.github.io/leaflet/
   output$cfmap <- renderLeaflet({
     leaflet(covid) %>%
       addTiles() %>%
+      # TODO marker should indicate availability
       addAwesomeMarkers(data = covid, lat = ~latitude, lng = ~longitude,
                         popup = ~provider_popup, clusterOptions = markerClusterOptions(),
                         icon = awesomeIcons(icon = 'ion-medkit', iconColor = 'darkgreen',
@@ -13,12 +15,12 @@ function(input, output) {
   # Display table using DT
   # see: https://rstudio.github.io/DT/
   output$covid <- DT::renderDataTable({
-    covid_display <- covid %>%
+    data_display <- covid %>%
       relocate(name, city, state, zip, phone, insurance_accepted, walkins_accepted, Moderna, Pfizer, Pfizer_child, Janssen) %>%
       ungroup() %>%
       select(name, city, state, zip, phone, insurance_accepted, walkins_accepted, Moderna, Pfizer, Pfizer_child, Janssen)
 
-    covid_display #return
+    data_display #return the data to display
   },
   filter = 'top',
   rownames = FALSE,
@@ -26,4 +28,26 @@ function(input, output) {
                "Accepts Insurance", "Walk-Ins Allowed",
                "Moderna (18+) In Stock", "Pfizer (12+) In Stock",
                "Pfizer (5-11) In Stock", "J&J Janssen (18+) In Stock"))
+
+  output$report <- downloadHandler(
+    # For PDF output, change this to "report.pdf"
+    filename = "report.html",
+    content = function(file) {
+      # Copy the report file to a temporary directory before processing it, in
+      # case we don't have write permissions to the current working dir (which
+      # can happen when deployed).
+      tempReport <- file.path(tempdir(), "report.Rmd")
+      file.copy("report.Rmd", tempReport, overwrite = TRUE)
+
+      # Set up parameters to pass to Rmd document
+      params <- list(n = input$slider  , state = input$state_selection)
+
+      # Knit the document, passing in the `params` list, and eval it in a
+      # child of the global environment (this isolates the code in the document
+      # from the code in this app).
+      rmarkdown::render(tempReport, output_file = file,
+                        params = params,
+                        envir = new.env(parent = globalenv())
+      )
+  })
 }
--- ui.R
+++ ui.R
@@ -1,14 +1,19 @@
 library(shinydashboard)
 library(shinycssloaders)
 
+
 dashboardPage(
   dashboardHeader(title = "COVID-19 Dashboard"),
   dashboardSidebar(collapsed = TRUE),
   dashboardBody(
     tabsetPanel(
-      tabPanel("Map", withSpinner(leafletOutput("cfmap", height = "600px"), size = 3)),
+      tabPanel("Map",
+               withSpinner(leafletOutput("cfmap", height = "600px"), size = 3),
+               sliderInput("slider", "Slider", 1, 100, 50),
+               selectInput("state_selection", "State", c("AK", "HI", "WA")),
+               downloadButton("report", "Generate report")),
       tabPanel("Data", DT::dataTableOutput("covid")),
-      tabPanel("About", includeMarkdown("./README.md"))
+      tabPanel("About", includeMarkdown("./TODO.Rmd"))
       )
     )
 )
