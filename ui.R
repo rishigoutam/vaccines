a08a8e4eeecbc01c4cc4969af55fd6308bf3a91d
rg <rishigoutam@users.noreply.github.com>
2022-02-12 22:11:00 -0800

    Sidebar visual tweaks
    -css hacks
    -filter shows only for Map and Data subitems in sidebar

--- ui.R
+++ ui.R
@@ -7,24 +7,25 @@ sidebar <- dashboardSidebar(
   sidebarMenu(
     # Setting id makes input$tabs give the tabName of currently-selected tab
     id = "tabs",
+    menuItem("About", tabName = "About", icon = icon("info-circle")),
     menuItem("Vaccine Dashboard", icon = icon('dashboard'), startExpanded = TRUE,
       menuSubItem("Providers Map", tabName = "Map", icon = icon("map")),
       menuSubItem("Data Table", tabName = "Data", icon = icon("table"))
     ),
-    menuItem("About", tabName = "About", icon = icon("info")),
     menuItem("Future Development", tabName = "FutureDev", icon = icon("code")),
     menuItem("://rishigoutam", icon = icon("github"), badgeLabel = "open",
              href = "https://www.github.com/rishigoutam/vaccines", newtab = TRUE),
-    menuItem(""),
-    menuItem("Filters", icon = icon("filter"))
+    menuItem("")
   ),
 
   # Show filters for the 'Maps' and 'Data' tabs only
   conditionalPanel(condition = "input.tabs == 'Map' || input.tabs == 'Data'",
+    menuItem("Filters", icon = icon("filter")),
     selectInput("state_input", "State/Territory", choices = states_list, multiple = TRUE),
     checkboxGroupInput("vaccine_type_input", "Vaccine in Stock",
                        choiceNames = c("J&J Janssen (18+)", "Moderna (18+)", "Pfizer (12+)", "Pfizer (5-11)"),
                        choiceValues = c("Janssen", "Moderna", "Pfizer", "Pfizer_child")),
+    checkboxGroupInput("preferences_title", "Provider Preferences"),
     div(id = "preferences",
         checkboxInput("insurance_input", "Accepts Insurance", value = FALSE),
         checkboxInput("walkin_input", "Walk-Ins Allowed", value = FALSE))
@@ -33,23 +34,22 @@ sidebar <- dashboardSidebar(
 
 body <- dashboardBody(
   tags$head(
-    tags$link(rel = "stylesheet", type = "text/css", href = "./www/vaccines.css"),
+    tags$link(rel = "stylesheet", type = "text/css", href = "vaccines.css"),
     tags$style("@import url(https://use.fontawesome.com/releases/v6.0.0/css/all.css);"),
   ),
 
   tabItems(
     tabItem("Map",
-             div(id = "num_provider_container",
-                 span(textOutput("num_providers", inline = TRUE), tags$i(icon('syringe', class = "fa-lg")))),
-             # h1(str_glue("{nrow(vaccine_data())} COVID-19 Vaccine Providers")),
-             withSpinner(leafletOutput("vaccine_map"), size = 1),
+             textOutput("num_providers"),
+             withSpinner(leafletOutput("vaccine_map"), type = 7),
 
              # Report Generation
              radioButtons("file_type_input", "Report File Type",
                           choices = c("PDF" = ".pdf", "HTML" = ".html"),
                           selected = ".pdf", inline = TRUE),
              downloadButton("report", "Generate report")),
-    tabItem("Data", DT::dataTableOutput("providers")),
+    tabItem("Data",
+            withSpinner(DT::dataTableOutput("providers"))),
     tabItem("About", includeMarkdown("./README.md")),
     tabItem("FutureDev", includeMarkdown("./TODO.Rmd"))
   )
